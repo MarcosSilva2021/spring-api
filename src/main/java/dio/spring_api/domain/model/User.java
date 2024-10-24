92846f9d01ffe8ce032bf75df25788913090fb95
Marcosdsj <mfsilva5585@gmail.com>
2024-10-24 15:46:17 -0300

    add User

---
+++ src/main/java/dio/spring_api/domain/model/User.java
@@ -0,0 +1,16 @@
+package dio.spring_api.domain.model;
+
+
+import java.util.List;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private Account account;
+    private Card card;
+    private List<Feature> features;
+    private List<News> news;
+
+
+}
