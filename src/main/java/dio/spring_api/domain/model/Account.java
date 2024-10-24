33434b47ed62a05b7b6e188948807e71314891ed
Marcos Silva <89165879+MarcosSilva2021@users.noreply.github.com>
2024-10-24 19:28:44 -0300

    add attr - Account

--- src/main/java/dio/spring_api/domain/model/Account.java
+++ src/main/java/dio/spring_api/domain/model/Account.java
@@ -1,4 +1,10 @@
 package dio.spring_api.domain.model;
 
 public class Account {
+
+    private Long id;
+    private String number;
+    private String agency;
+    private BigDecimal balance;
+    private BigDecimal limit;
 }
