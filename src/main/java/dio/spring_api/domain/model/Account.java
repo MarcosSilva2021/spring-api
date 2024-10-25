7170f3b70d93355cb55ae9025328df182bcce64d
Marcosdsj <mfsilva5585@gmail.com>
2024-10-25 15:13:13 -0300

    add gets and sets

--- src/main/java/dio/spring_api/domain/model/Account.java
+++ src/main/java/dio/spring_api/domain/model/Account.java
@@ -1,5 +1,7 @@
 package dio.spring_api.domain.model;
 
+import java.math.BigDecimal;
+
 public class Account {
 
     private Long id;
@@ -7,4 +9,44 @@ public class Account {
     private String agency;
     private BigDecimal balance;
     private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
 }
