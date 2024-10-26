8442db8af59a11ae3e75f0f4d2a2ed4a112d66f0
Marcosdsj <mfsilva5585@gmail.com>
2024-10-26 20:55:14 -0300

    add metodo findById

--- src/main/java/dio/spring_api/service/UserService.java
+++ src/main/java/dio/spring_api/service/UserService.java
@@ -1,4 +1,10 @@
 package dio.spring_api.service;
 
+import dio.spring_api.domain.model.User;
+
 public interface UserService {
+
+    User findById(Long id);
+
+    User create(User userToCreate);
 }
--- src/main/java/dio/spring_api/service/impl/UserServiceImpl.java
+++ src/main/java/dio/spring_api/service/impl/UserServiceImpl.java
@@ -1,6 +1,30 @@
 package dio.spring_api.service.impl;
 
+import dio.spring_api.domain.model.User;
+import dio.spring_api.domain.repository.UserRepository;
 import dio.spring_api.service.UserService;
+import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+
+@Service
 public class UserServiceImpl implements UserService {
+
+    // injetando atributos
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        return null;
+    }
 }
