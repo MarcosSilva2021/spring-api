4eca7fff1b64970a6ebf09fa4bb4c92f98939e0f
Marcosdsj <mfsilva5585@gmail.com>
2024-10-26 11:41:51 -0300

    add Repository - conf bd h2

---
+++ src/main/java/dio/spring_api/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dio.spring_api.domain.repository;
+
+import dio.spring_api.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
