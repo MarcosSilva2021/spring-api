a306587f31d5860a366c9bee00f4aea7e69aa3a4
Marcosdsj <mfsilva5585@gmail.com>
2024-10-26 20:36:34 -0300

    creating service layer

---
+++ src/main/java/dio/spring_api/service/UserService.java
@@ -0,0 +1,4 @@
+package dio.spring_api.service;
+
+public interface UserService {
+}
---
+++ src/main/java/dio/spring_api/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package dio.spring_api.service.impl;
+
+import dio.spring_api.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
