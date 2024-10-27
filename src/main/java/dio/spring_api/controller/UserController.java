cadbde031cd049637373c4d7f9d8c76954b8468b
Marcosdsj <mfsilva5585@gmail.com>
2024-10-26 21:32:03 -0300

    add metodo Controller GetMapping (id)

---
+++ src/main/java/dio/spring_api/controller/UserController.java
@@ -0,0 +1,27 @@
+package dio.spring_api.controller;
+
+import dio.spring_api.domain.model.User;
+import dio.spring_api.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findByid(@PathVariable Long id){
+        vr user = userService.findById(id);
+        return  ResponseEntity.ok(user);
+    }
+
+}
