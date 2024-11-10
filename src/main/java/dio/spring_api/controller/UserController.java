7e3b8ff5d661b7e0190ed28d3a237a8d5b129721
Marcosdsj <mfsilva5585@gmail.com>
2024-11-09 22:09:46 -0300

    add doc API with swagger.

--- src/main/java/dio/spring_api/controller/UserController.java
+++ src/main/java/dio/spring_api/controller/UserController.java
@@ -2,6 +2,10 @@ package dio.spring_api.controller;
 
 import dio.spring_api.domain.model.User;
 import dio.spring_api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Spring API - Users", description = "Documentação da API de gerenciamento de usuários.")
 public class UserController {
 
     private final UserService userService;
@@ -20,12 +25,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Buscar um usuário por ID", description = "Retorna um usuário baseado no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         var user = userService.findById(id);
         return  ResponseEntity.ok(user);
     }
 
+    @Operation(summary = "Criar um novo usuário", description = "Cria um novo usuário no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @PostMapping
     public ResponseEntity<User> create(@RequestBody User userToCreate){
         var userCreated = userService.create(userToCreate);
@@ -36,18 +51,30 @@ public class UserController {
         return  ResponseEntity.created(location).body(userCreated);
     }
 
+    @Operation(summary = "Atualizar um usuário existente", description = "Atualiza as informações de um usuário existente.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user ){
         User updateUser = userService.update(id, user);
         return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
 
+    @Operation(summary = "Deletar um usuário", description = "Deleta um usuário com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         userService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Buscar todos os usuários", description = "Retorna todos os usuários cadastrados.")
+    @ApiResponse(responseCode = "200", description = "Lista de usuários retornada com sucesso")
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> users = userService.findAll();
