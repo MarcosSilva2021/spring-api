a756878a92b0b5907ea5e04cf1a9fcb39b08c058
Marcosdsj <mfsilva5585@gmail.com>
2024-11-11 16:10:29 -0300

    add class CardDTO.

--- src/main/java/dio/spring_api/domain/dto/CardDTO.java
+++ src/main/java/dio/spring_api/domain/dto/CardDTO.java
@@ -1,4 +1,16 @@
 package dio.spring_api.domain.dto;
 
-public record CardDTO() {
+import dio.spring_api.domain.model.Card;
+
+import java.math.BigDecimal;
+
+public record CardDTO(Long id, String number, BigDecimal limit) {
+
+    public Card toModel(){
+        Card model = new Card();
+        model.setId(this.id);
+        model.setNumber(this.number);
+        model.setLimit(this.limit);
+        return model;
+    }
 }
