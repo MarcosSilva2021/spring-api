2e4dd60d5898bc60dd26cc048cb5456d6af7a311
Marcosdsj <mfsilva5585@gmail.com>
2024-11-11 16:18:23 -0300

    add class FeatureDTO.

--- src/main/java/dio/spring_api/domain/dto/FeatureDTO.java
+++ src/main/java/dio/spring_api/domain/dto/FeatureDTO.java
@@ -1,4 +1,18 @@
 package dio.spring_api.domain.dto;
 
-public record FeatureDTO() {
+import dio.spring_api.domain.model.Feature;
+
+public record FeatureDTO(Long id, String icon, String description) {
+
+    public FeatureDTO(Feature model){
+        this(model.getId(), model.getIcon(), model.getDescription());
+    }
+
+    public Feature toModel(){
+        Feature model = new Feature();
+        model.setId(this.id);
+        model.setIcon(this.icon);
+        model.setDescription(this.description);
+        return model;
+    }
 }
