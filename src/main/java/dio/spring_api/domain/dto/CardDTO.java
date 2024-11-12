3d40a39d3aa5fb037db0af345125daef762955bc
Marcosdsj <mfsilva5585@gmail.com>
2024-11-12 16:22:29 -0300

    add class UserDTO.

--- src/main/java/dio/spring_api/domain/dto/CardDTO.java
+++ src/main/java/dio/spring_api/domain/dto/CardDTO.java
@@ -6,6 +6,10 @@ import java.math.BigDecimal;
 
 public record CardDTO(Long id, String number, BigDecimal limit) {
 
+    public CardDTO(Card model){
+        this(model.getId(), model.getNumber(), model.getLimit());
+    }
+
     public Card toModel(){
         Card model = new Card();
         model.setId(this.id);
